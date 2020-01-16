1a4e6aa0727d771447fa725c3e2324737316a5cd
Christophe Serafin <christophe.serafin@gmail.com>
2020-01-16 14:43:04 -0800

    new: add support for activities related to enforcer

--- main.go
+++ main.go
@@ -44,19 +44,32 @@ func main() {
 	eventsPubs := make(chan *bahamut.Publication)
 	pubsub.Subscribe(eventsPubs, eventsErrs, "squall-events", bahamut.NATSOptSubscribeQueue(cfg.NATSQueueName))
 
+	// Activities also named Audit logs
+	activitiesErrs := make(chan error)
+	activitiesPubs := make(chan *bahamut.Publication)
+	pubsub.Subscribe(activitiesPubs, activitiesErrs, "activities", bahamut.NATSOptSubscribeQueue(cfg.NATSQueueName))
+
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	for {
 		select {
 
+		// Squall events
 		case p := <-eventsPubs:
 			if errSend := sendPublication(p); errSend != nil {
 				fmt.Println("sendPublication error: ", errSend)
 			}
-
 		case err := <-eventsErrs:
 			fmt.Printf("Got an event error %s\n", err)
+
+		// Activities
+		case p := <-activitiesPubs:
+			if errSend := sendActivity(p); errSend != nil {
+				fmt.Println("sendActivity error: ", errSend)
+			}
+		case err := <-activitiesErrs:
+			fmt.Printf("Got an activity error %s\n", err)
 
 		case <-ctx.Done():
 			fmt.Println("Exit")
@@ -64,9 +77,10 @@ func main() {
 	}
 }
 
-// sendPublication sends a publication
+// sendPublication sends a publication based on an event.
 func sendPublication(pub *bahamut.Publication) error {
 
+	// Convert the publication to an Aporeto object
 	obj, err := publicationToIdentifiable(pub)
 	if err != nil {
 		return err
@@ -74,6 +88,7 @@ func sendPublication(pub *bahamut.Publication) error {
 
 	switch obj.Identity() {
 
+	// For now, we are only listening to enforcer events
 	case gaia.EnforcerIdentity:
 		enforcer := obj.(*gaia.Enforcer)
 		return sendEnforcerReport(enforcer)
@@ -85,20 +100,20 @@ func sendPublication(pub *bahamut.Publication) error {
 	}
 }
 
-// publicationToString converts a publication to a readable string
+// publicationToString converts a publication to an identifiable
 func publicationToIdentifiable(pub *bahamut.Publication) (elemental.Identifiable, error) {
 
 	// Decode the event of the publication
 	event := &elemental.Event{}
 
 	if err := pub.Decode(event); err != nil {
-		return nil, fmt.Errorf("unable to decode the event %s", err)
+		return nil, fmt.Errorf("unable to decode the publication: %s", err)
 	}
 
 	// Decode the identifiable
 	identifiable := gaia.Manager().IdentifiableFromString(event.Identity)
 	if err := event.Decode(&identifiable); err != nil {
-		return nil, fmt.Errorf("unable to decode the identifiable %s", err)
+		return nil, fmt.Errorf("unable to decode the event: %s", err)
 	}
 
 	return identifiable, nil
@@ -114,6 +129,29 @@ func sendEnforcerReport(enforcer *gaia.Enforcer) error {
 	Operational Status: %s
 	Enforcement Status: %s
 `, enforcer.ID, enforcer.Name, enforcer.Namespace, enforcer.OperationalStatus, enforcer.EnforcementStatus)
+
+	return nil
+}
+
+// sendActivity sends an activity from a publication.
+func sendActivity(pub *bahamut.Publication) error {
+
+	activity := gaia.NewActivity()
+
+	if err := pub.Decode(activity); err != nil {
+		return fmt.Errorf("unable to decode the activity: %s", err)
+	}
+
+	if activity.TargetIdentity != gaia.EnforcerIdentity.Name {
+		return fmt.Errorf("skipping activity for target identity: %s", activity.TargetIdentity)
+	}
+
+	fmt.Printf(`--- Activity to be send to Kafka:
+	ID: %s
+	Date: %s
+	Namespace: %s
+	Diff: %s
+`, activity.ID, activity.Date, activity.Namespace, activity.Diff)
 
 	return nil
 }
